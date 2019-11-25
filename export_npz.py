7709ad4bc8052d5f623e22f6c0ed699b4c95f329
Lyu Ying <lyuying@Lyus-MacBook-Pro.local>
2019-11-24 23:36:52 -0600

    export 64*64 .jpg

--- export_npz.py
+++ export_npz.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
 import os, time
 import matplotlib.pyplot as plt
 plt.switch_backend('Agg')
@@ -8,10 +14,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
 from torch.autograd import Variable
 import torchvision.datasets.mnist as mnist
 import numpy 
+from skimage import transform,data
+from scipy import misc
+
+
+# In[2]:
+
 
 
 # G(z)
@@ -44,27 +55,36 @@ class generator(nn.Module):
         x = F.tanh(self.deconv5(x))
 
         return x
-
-
 def normal_init(m, mean, std):
     if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
         m.weight.data.normal_(mean, std)
         m.bias.data.zero_()
 
-        
-p = "./data/raw/"
-test_torch = mnist.read_image_file(os.path.join(p,'t10k-images-idx3-ubyte'))
-tor2numpy=test_set.numpy()
-# tor2numpy.shape
+
+# In[3]:
+
+
 
 if not os.path.isdir('MNIST_DCGAN_results'):
     os.mkdir('MNIST_DCGAN_results')
-if not os.path.isdir('MNIST_DCGAN_results/Metric'):
-    os.mkdir('MNIST_DCGAN_results/Metric')
-np.save('MNIST_DCGAN_results/Metric/mnist_test.npy',tor2numpy)
+if not os.path.isdir('MNIST_DCGAN_results/mnist'):
+    os.mkdir('MNIST_DCGAN_results/mnist')
+test_torch = mnist.read_image_file(os.path.join('data/raw/','t10k-images-idx3-ubyte'))
+# test_torch.size()
+tor2numpy=test_torch.numpy()
+# tor2numpy.shape
+# %matplotlib inline
+# plt.figure(figsize=(64,64),dpi=1)
+# plt.imshow(tor2numpy[0], cmap='gray')
 
-a=np.load('MNIST_DCGAN_results/Metric/mnist_test.npy')
-# a.shape
+for i in range(0,1000):
+    dst=transform.resize(tor2numpy[i], (64, 64))
+    misc.imsave('MNIST_DCGAN_results/mnist/'+str(i)+".jpg",dst)
+
+
+
+# In[4]:
+
 
 
 
@@ -77,6 +97,10 @@ small_G = generator(small_size)
 G.cuda()
 small_G.cuda()
 
+
+# In[5]:
+
+
 if os.path.exists("MNIST_DCGAN_results/state.pkl"):
     checkpoint = torch.load("MNIST_DCGAN_results/state.pkl")
     G.load_state_dict(checkpoint['G'])
@@ -85,19 +109,43 @@ if os.path.exists("MNIST_DCGAN_results/state_small_"+ str(small_size)+ ".pkl"):
     small_G.load_state_dict(checkpoint['small_G'])
 
 
-z_ = torch.randn((5*5, 100)).view(-1, 100, 1, 1)
+# In[13]:
+
+
+z_ = torch.randn((1000, 100)).view(-1, 100, 1, 1)
 z_ = Variable(z_.cuda(), volatile=True)
 G.eval()
 small_G.eval()
 test_images = G(z_)
 test_images_small = small_G(z_)
 
+
+# In[14]:
+
+
+
 images_numpy = test_images.cpu().data.numpy()
 # images_numpy.shape
+
+
+# In[15]:
+
+
 images_small_numpy = test_images_small.cpu().data.numpy()
 # images_small_numpy.shape
 
-# get_ipython().magic('matplotlib inline')
-plt.imshow(test_images[2, 0].cpu().data.numpy(), cmap='gray')
-plt.show()
+
+# In[16]:
+
+
+if not os.path.isdir('MNIST_DCGAN_results/G'):
+    os.mkdir('MNIST_DCGAN_results/G')
+if not os.path.isdir('MNIST_DCGAN_results/small_G_'+ str(small_size)):
+    os.mkdir('MNIST_DCGAN_results/small_G_'+ str(small_size))
+
+for i in range(0,1000):
+    dst=transform.resize(images_numpy[i][0], (64, 64))
+    dst_small = transform.resize(images_small_numpy[i][0], (64, 64))
+    misc.imsave('MNIST_DCGAN_results/G/'+str(i)+".jpg",dst)
+    misc.imsave('MNIST_DCGAN_results/small_G_'+ str(small_size)+'/'+str(i)+".jpg",dst_small)
 
